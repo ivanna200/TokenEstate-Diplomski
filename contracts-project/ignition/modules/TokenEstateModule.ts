29447572bcebc839e5679a46e5fa85ba5cc7c80d
ivanna200 <ivana03lazovic@gmail.com>
2026-07-18 15:46:57 +0200

    Azuriran deployment modul - dodat Marketplace, sva 3 ugovora deployovana lokalno

--- contracts-project/ignition/modules/TokenEstateModule.ts
+++ contracts-project/ignition/modules/TokenEstateModule.ts
@@ -1,18 +1,13 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("TokenEstateModule", (m) => {
-  // Nalog koji deployuje ugovore postaje i njihov inicijalni vlasnik (owner)
   const deployer = m.getAccount(0);
 
-  // 1. Deployuj PropertyNFT ugovor
   const propertyNFT = m.contract("PropertyNFT", [deployer]);
-
-  // 2. Deployuj TokenFactory, povezan sa adresom PropertyNFT ugovora
   const tokenFactory = m.contract("TokenFactory", [propertyNFT, deployer]);
+  const marketplace = m.contract("Marketplace", []);
 
-  // 3. Prenesi vlasništvo nad PropertyNFT na TokenFactory,
-  //    da bi TokenFactory mogao pozivati markAsTokenized()
   m.call(propertyNFT, "transferOwnership", [tokenFactory]);
 
-  return { propertyNFT, tokenFactory };
+  return { propertyNFT, tokenFactory, marketplace };
 });
